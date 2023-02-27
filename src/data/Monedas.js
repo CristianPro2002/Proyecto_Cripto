940cc30c8ab3f5b738e7eae5d483cddf2d58f333
cristianyt2 <cristian.0713.cs@gmail.com>
2023-02-26 20:54:30 -0500

    first commit

---
+++ src/data/Monedas.js
@@ -0,0 +1,27 @@
+
+const Monedas = [
+    {
+        id: "USD",
+        nombre: "Dolar de Estados Unidos",
+      },
+      {
+        id: "MXN",
+        nombre: "Peso Mexicano",
+      },
+      {
+        id: "EUR",
+        nombre: "Euro",
+      },
+      {
+        id: "GBP",
+        nombre: "Libra Esterlina",
+      },
+      {
+        id: "COP",
+        nombre: "Peso Colombiano",
+      },
+]
+
+export {
+    Monedas
+}
